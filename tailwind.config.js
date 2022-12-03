bb12b81427ecda1efa8a5f0de4e4265e661d7b26
Abir Santra <santraabir8@gmail.com>
2022-12-03 10:34:40 +0530

    configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			Inter: ["Inter", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				"primary-dark": "#ff5400",
+				primary: "#e85d04",
+				"primary-light": "#ff7900",
+				"primary-extralight": "#FBE2D0",
+				"white-one": "#ffffff",
+				"white-two": "#f8f9fa",
+				"grey-one": "#e9ecef",
+				"grey-two": "#ced4da",
+				"grey-three": "#adb5bd",
+				"black-one": "#212529",
+				"black-two": "#343a40",
+			},
+		},
 	},
 	plugins: [],
 };
