01f1845ef52f3270080d7075bd8779557f1957a3
Abir Santra <santraabir8@gmail.com>
2022-12-04 11:14:33 +0530

    added homepage

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Announcement from "../components/Announcement";
+
+const Homepage = () => {
+	return (
+		<div>
+			<Announcement text="Grand Opening Sale! Get 50% off on selected products" />
+		</div>
+	);
+};
+
+export default Homepage;
