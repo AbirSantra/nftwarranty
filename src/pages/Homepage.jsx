fd460865c83a94e59d99a6e9a09d74c578eaf00e
Abir Santra <santraabir8@gmail.com>
2022-12-09 00:18:36 +0530

    designed footer

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,6 +3,7 @@ import Announcement from "../components/Announcement";
 import BestDeals from "../components/BestDeals";
 import Categories from "../components/Categories";
 import Features from "../components/Features";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
 
@@ -15,6 +16,7 @@ const Homepage = () => {
 			<Categories />
 			<BestDeals />
 			<Features />
+			<Footer />
 		</div>
 	);
 };
