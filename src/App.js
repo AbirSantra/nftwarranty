833d447f4760e90017b6c6d2215479960702af30
Abir Santra <santraabir8@gmail.com>
2022-12-10 18:34:45 +0530

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Homepage from "./pages/Homepage";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
-			<Homepage />
+			<Routes>
+				<Route path="/" element={<Navigate to="home" />} />
+				<Route path="/home" element={<Homepage />} />
+			</Routes>
 		</div>
 	);
 }
