9facde82000ee4cb570c0f738969e78fd74966db
Abir Santra <santraabir8@gmail.com>
2022-12-11 11:01:47 +0530

    added authentication page routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Homepage from "./pages/Homepage";
 import { Routes, Route, Navigate } from "react-router-dom";
+import Authpage from "./pages/Authpage";
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Navigate to="home" />} />
 				<Route path="/home" element={<Homepage />} />
+				<Route path="/auth" element={<Authpage />} />
 			</Routes>
 		</div>
 	);
