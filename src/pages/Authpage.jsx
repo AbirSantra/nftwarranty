042393da18f820894f7e15140e00e6299d41d554
Abir Santra <santraabir8@gmail.com>
2022-12-12 20:20:00 +0530

    created auth page basic layout

--- src/pages/Authpage.jsx
+++ src/pages/Authpage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Authpage = () => {
-	return <div>This is the login/register page</div>;
+	return (
+		<div className="authpage section">
+			<div className="authpage--content w-full flex justify-center items-center">
+				{/* Left Section */}
+				<div className="auth--form--container section w-1/2 min-h-screen">
+					Login- Register form goes here
+				</div>
+				{/* Right Section */}
+				<div className="auth--image--container section w-1/2 min-h-screen bg-neutral-100">
+					Authentication Image goes here
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Authpage;
