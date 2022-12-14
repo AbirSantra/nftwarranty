e2b860a83d0ce3d649216ca0826ce0aefa88020f
Abir Santra <santraabir8@gmail.com>
2022-12-14 18:20:33 +0530

    added logo and copyright text

--- src/pages/Authpage.jsx
+++ src/pages/Authpage.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Authpage = () => {
 	return (
 		<div className="authpage section">
 			<div className="authpage--content w-full flex justify-center items-center">
 				{/* Left Section */}
-				<div className="auth--form--container section w-1/2 min-h-screen">
+				<div className="auth--form--container flex-1 flex flex-col justify-between min-h-screen px-16 py-8">
 					{/* Company Logo */}
+					<Link to="/home" className="font-bold text-2xl text-black-one">
+						Shopus
+					</Link>
 					{/* Form */}
 					{/* Copyright */}
+					<p className="text-grey-three">
+						Shopus &copy; {new Date().getFullYear()}. All rights reserved.
+					</p>
 				</div>
 				{/* Right Section */}
-				<div className="auth--image--container section w-1/2 min-h-screen bg-slate-100">
+				<div className="auth--image--container section flex-1 min-h-screen bg-slate-100 p-16">
 					<span className="circle h-40 w-40 bg-primary shadow-lg rounded-full"></span>
 				</div>
 			</div>
