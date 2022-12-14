4298094b629bc64ff65360fc2ee5d5bde5c2323e
Abir Santra <santraabir8@gmail.com>
2022-12-14 18:14:00 +0530

    added placeholder auth image

--- src/pages/Authpage.jsx
+++ src/pages/Authpage.jsx
@@ -6,11 +6,13 @@ const Authpage = () => {
 			<div className="authpage--content w-full flex justify-center items-center">
 				{/* Left Section */}
 				<div className="auth--form--container section w-1/2 min-h-screen">
-					Login- Register form goes here
+					{/* Company Logo */}
+					{/* Form */}
+					{/* Copyright */}
 				</div>
 				{/* Right Section */}
-				<div className="auth--image--container section w-1/2 min-h-screen bg-neutral-100">
-					Authentication Image goes here
+				<div className="auth--image--container section w-1/2 min-h-screen bg-slate-100">
+					<span className="circle h-40 w-40 bg-primary shadow-lg rounded-full"></span>
 				</div>
 			</div>
 		</div>
