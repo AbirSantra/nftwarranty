f8db33af424043e4c207755552114fe31b8bc7e4
Abir Santra <santraabir8@gmail.com>
2023-11-27 16:57:10 +0530

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		fontFamily: {
-			Inter: ["Inter", "sans-serif"],
-		},
-		extend: {
-			colors: {
-				"primary-dark": "#ff5400",
-				primary: "#FB6A00",
-				"primary-light": "#ff7900",
-				"primary-extralight": "#FBE2D0",
-				"white-one": "#ffffff",
-				"white-two": "#f8f9fa",
-				"grey-one": "#e9ecef",
-				"grey-two": "#ced4da",
-				"grey-three": "#adb5bd",
-				"black-one": "#212529",
-				"black-two": "#343a40",
-			},
-		},
-	},
-	plugins: [require("@tailwindcss/aspect-ratio")],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        "[auto,auto,1fr]": "auto auto 1fr",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+  ],
 };
