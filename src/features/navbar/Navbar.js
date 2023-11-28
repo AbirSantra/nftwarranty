f9c0648ba831e6095cdd0158acd99b4272ecca13
Abir Santra <santraabir8@gmail.com>
2023-11-28 22:38:31 +0530

    added connect wallet feature

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
@@ -29,6 +29,26 @@ function classNames(...classes) {
 function NavBar({ children }) {
   const items = useSelector(selectItems);
   const userInfo = useSelector(selectUserInfo);
+
+  const [Wallet, setWallet] = useState(null);
+
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      console.log("Meta Mask Dectected");
+
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWallet(accounts[0]);
+        console.log(accounts);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("Install Meta Mask");
+    }
+  };
 
   return (
     <>
@@ -121,6 +141,21 @@ function NavBar({ children }) {
                             leaveTo="transform opacity-0 scale-95"
                           >
                             <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <div
+                                    onClick={connectWallet}
+                                    className={classNames(
+                                      active ? "bg-gray-100" : "",
+                                      "block px-4 py-2 text-sm text-gray-700 cursor-pointer truncate"
+                                    )}
+                                  >
+                                    {Wallet
+                                      ? `Wallet connected: ${Wallet}`
+                                      : "Connect Wallet"}
+                                  </div>
+                                )}
+                              </Menu.Item>
                               {userNavigation.map((item) => (
                                 <Menu.Item key={item.name}>
                                   {({ active }) => (
